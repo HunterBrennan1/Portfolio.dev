aaf55ded75622b56002d22a97a8e36045abe3e53
Hunter <hunterbrennan15@gmail.com>
2023-06-29 00:28:33 -0400

    added listed items for navbar

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
     <div className="App">
-
+      <Navbar />
     </div>
   );
 }
--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -1,10 +1,18 @@
 
 
-AOS.init();
-
 export const Navbar = () => {
   return (
-    <section>
+    <section className="navbar_wrapper">
+      <header>
+        <nav>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About me</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </nav>
+      </header>
     </section>
   )
 };

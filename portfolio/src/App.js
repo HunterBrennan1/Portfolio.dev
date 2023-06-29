f5d14133527034ec2c2c7c597036cbd46972ecbb
Hunter <hunterbrennan15@gmail.com>
2023-06-29 01:38:14 -0400

    added banner text

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import Navbar from './components/Navbar';
+import HeroImg from './components/Hero';
 
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <HeroImg />
     </div>
   );
 }
---
+++ portfolio/src/components/Hero.js
@@ -0,0 +1,16 @@
+// import HeroImg from "../assets/heroimg.PNG";
+
+export const Hero = () => {
+  return (
+    <section className="hero_container">
+      <div>
+        <h1 className="banner_title_1">Hey, I'm <span className="banner_name_span">Hunter Brennan</span>.</h1>
+        <h2 className="banner_title_2">I help you build outstanding <span className="banner_website_span">websites.</span></h2>
+      </div>
+
+    </section>
+
+  )
+};
+
+export default Hero;
